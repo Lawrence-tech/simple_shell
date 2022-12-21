730a80dd39ef0a1cb31f05af86b827d09b88c63a
Lawrence-tech <lawrencesagini2@gmail.com>
2022-12-20 22:26:47 -0800

    shell functions

---
+++ errors.c
@@ -0,0 +1,77 @@
+#include "shell.h"
+
+/**
+  *_eputs - prints input string
+  *@str: string to be printed
+  */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] 1 = '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+/**
+  *_eputchar - writes the character c to stderr
+  *@c: character to print
+  *Return: 1,success. -1, failure. errno set appropriately
+  */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+  *_putfd - writes c to given fd
+  *@c: character input
+  *@fd: filedecriptor
+  *Return: 1,success. -1, failure. errno set appropriately
+  */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+  *_putsfd - prints an input string
+  *@str: string to printed
+  *@fd: filedesriptor to write to
+  *Return: number of chars put
+  */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putsfd(*str++, fd);
+	}
+	return (i);
+}
